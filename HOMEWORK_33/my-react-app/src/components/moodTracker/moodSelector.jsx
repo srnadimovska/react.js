d2f9b2433cf2cd23dd3b619b59b90f8b75ac99a7
srnadimovska <srna.trajkovska@gmail.com>
2025-04-02 00:04:01 +0200

    novi

---
+++ HOMEWORK_33/my-react-app/src/components/moodTracker/moodSelector.jsx
@@ -0,0 +1,16 @@
+function MoodSelector({ moodChange }) {
+  const moods = ["Happy", "Sad", "Excited", "Tired"];
+
+  return (
+    <div>
+      <h2>Select your mood:</h2>
+      {moods.map((mood) => (
+        <button key={mood} onClick={() => moodChange(mood)}>
+          {mood}
+        </button>
+      ))}
+    </div>
+  );
+}
+
+export default MoodSelector;
